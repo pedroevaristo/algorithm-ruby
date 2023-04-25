b85d73e41d138eba855ae20b76e64626972363f9
pedroevaristo <hattoriminamoto@gmail.com>
2023-04-25 20:19:33 -0300

    25/04/23

--- AlgorithmRuby/Q01.rb
+++ AlgorithmRuby/Q01.rb
@@ -6,22 +6,56 @@
 puts "Welcome to The Calculator:\n"
 puts "Type numbers and operator"
 puts "-------------\n| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |\n|   | 0 |   |\n-------------"
-puts "Type two numbers"
 
-count = 1
 
+
+countInCase = 0
+loopStock = []
+resultCalculate = 0
+count = 0
 loop do
+    
     option = gets
-    # if option.include?'exit'
-    #     # puts "true"
-    #     break
-    # end
-    if count == 2
-        puts "Entrou no = a 2"
-    end
+    loopStock[count] = option.to_i
     count += 1
-end
-
-def sum()
+    if option.include?'exit'
+         
+         break
+    end
+    
+    if count == 2
+       #switch
+       #puts "#{loopStock[count] + loopStock[count]}"
+       puts "Do you want: ( + ),( - ),( / ) or ( * )"
+       
+       chooseOperator = gets.to_s
+       case chooseOperator.chomp
+       
+        
+        when '+'
+            puts"#{loopStock[0] + loopStock[1]}"
+            countInCase+=1
+        when '-'
+            puts"#{loopStock[0] - loopStock[1]}"
+            countInCase+=1
+            
+        when '/'
+            puts"#{loopStock[0] / loopStock[1]}"
+            countInCase+=1
+            
+        when '*'
+            puts"#{loopStock[0] * loopStock[1]}"
+            countInCase+=1
+        end
+        if countInCase == 1
+            count = 0
+            puts "Type two numbers or would you liek to leave?(exit)"
+            next
+        end
+    end
     
 end
+
+# def sum()
+    
+# end
