ca8224404b48cfda3d21105b946e8e2408171084
pedroevaristo <hattoriminamoto@gmail.com>
2023-05-22 21:59:16 -0300

    22/05/2023/21:59

--- AlgorithmRuby/Q02.1.rb
+++ AlgorithmRuby/Q02.1.rb
@@ -1,2 +1,9 @@
-# 1°Utilizando uma collection do tipo array, escreva um programa que receba 3
+# 1° Utilizando uma collection do tipo array, escreva um programa que receba 3
 # números e no final exiba o resultado de cada um deles elevado a segunda potência
+collection = []
+print "Digite três números, abaixo:\n"
+for loop in 0..2
+    collection [loop] = gets.chomp.to_i
+end
+
+puts "#{collection}, testando"
