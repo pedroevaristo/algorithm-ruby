dc781670bdff7bf47a1197634ee259bea071e1e4
pedroevaristo <hattoriminamoto@gmail.com>
2023-05-25 11:40:28 -0300

    25/05/2023/11:40

--- AlgorithmRuby/Q02.2.rb
+++ AlgorithmRuby/Q02.2.rb
@@ -1,3 +1,17 @@
-# 2°crie uma collectiondo tipo hash e permita que o usuário crie três elementos
-# informandoa chave e o valor. No final do programa para cada um desses elementos
+# 2°crie uma collection do tipo hash e permita que o usuário crie três elementos
+# informando a chave e o valor. No final do programa para cada um desses elementos
 # imprima a frase "uma das chaves é **** e o seu valor é ***"
+
+hash_Receive = Hash.new
+loopUntil = 1
+until loopUntil == 4
+    
+    inputhash = gets.chomp
+    hash_Receive[loopUntil] = inputhash
+    
+    loopUntil+=1
+end
+
+hash_Receive.each do |key, value|
+    puts "Uma das chaves é #{key} e o seu valor é #{value}"
+end
--- AlgorithmRuby/Q02.3.rb
+++ AlgorithmRuby/Q02.3.rb
@@ -3,4 +3,4 @@
 # crie uma instrução que seleciona o maior valor deste hash e no final imprima a chave e valor 
 # do elemento resultante
 
-
+Numbers = {a:10,b:30,c:20,d:25,e:15}
